9b32f58d8cc5a6c87a2d9d8339d11d09b6c6bb8f
Ali Nauroze <alinauroze@hotmail.com>
2021-04-25 00:25:52 +0500

    print.c

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int x,y,a;
+	printf("enter the value of x:");
+	scanf("%d", &x);
+	printf("enter the value of y:");
+	scanf("%d", &y);
+	a=x+y;
+	printf("The value of a is %d",a);
+}
